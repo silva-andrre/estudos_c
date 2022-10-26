a07576957b84fc0cb47996a2be2baa0f5052606a
Andre Silva <asilva22@uolinc.com>
2022-10-26 09:20:05 -0300

    Versão 0.2 da Cifra de Cesar

--- Cifra/cifra_cesar.c
+++ Cifra/cifra_cesar.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char* argv[]){
 
     //define as constantes que utilizaremos para rotacionar (ROTx) as letras 
-    #define tamTexto 4
-    #define qtdLetra 26
+    //#define TAM_TEXTO 4
+    #define QTD_LETRA 26
 
     //definicao de variaveis
-    int i = 0, j = 0;
-    char vetorTexto[tamTexto];
+    int rot_texto = 0;
+    int vetorTexto[rot_texto];
     char vetorLetras[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
+    printf("Digite um número inteiro para definir o rotacionamento: ");
+    scanf ("%d", &vetorTexto[rot_texto]);
+
+/*
     //Lendo o texto a ser encriptado
-    for(i=0; i < tamTexto; i++){
-        scanf ("%c", &vetorTexto[tamTexto]);
+    for(i=0; i < TAM_TEXTO; i++){
+        scanf ("%c", &vetorTexto[TAM_TEXTO]);
     }
 
-    for (i = 0; i < tamTexto; i++){
-        for (j = 0; j < qtdLetra; j++){
+    for (i = 0; i < TAM_TEXTO; i++){
+        for (j = 0; j < QTD_LETRA; j++){
 
-            if (vetorTexto[i] == vetorLetras[j] && j+ 3 < qtdLetra){
+            if (vetorTexto[i] == vetorLetras[j] && j+ 3 < QTD_LETRA){
                 vetorTexto[i] = vetorLetras[j+ 3];
             }
             else{
                 j = j + 3;
-                j = j - qtdLetra;
+                j = j - QTD_LETRA;
 
                 vetorTexto[i] = vetorLetras[j];
             }
         }
     }
 
-    for (i = 0; i < tamTexto; i++){
+    for (i = 0; i < TAM_TEXTO; i++){
         printf("%c", vetorTexto[i]);
     }
-    
+  */  
     return 0;
 
 }
